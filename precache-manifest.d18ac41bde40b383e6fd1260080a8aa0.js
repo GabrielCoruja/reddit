b7a509c901b457e67dbbdbd78cf684baed99e3b7
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-03-15 21:42:23 -0300

    Updates

---
+++ precache-manifest.d18ac41bde40b383e6fd1260080a8aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b69d348c0097a33d133f1136c3f942b",
+    "url": "/reddit/index.html"
+  },
+  {
+    "revision": "1361c45ed81938732094",
+    "url": "/reddit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "540199934c35960e13a0",
+    "url": "/reddit/static/js/2.f0b3b9b7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reddit/static/js/2.f0b3b9b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1361c45ed81938732094",
+    "url": "/reddit/static/js/main.da382203.chunk.js"
+  },
+  {
+    "revision": "c4333b37e82d1c8815d7",
+    "url": "/reddit/static/js/runtime-main.670f4eef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit/static/media/logo.5d5d9eef.svg"
+  }
+]);
